96b9b97469d0cdf443fe1f6c0ed91c11ac9cdafe
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-03-01 10:43:29 +0100

    Add files via upload

---
+++ MadLibs.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+   <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+   <title>Mad Libs</title>
+   <link rel="stylesheet" type="text/css" href="MadLibs.css">
+</head>
+<body>
+<div class="container">
+    <nav>
+        <ul>
+            <li><a href="MadLibs.php">Er heerst paniek...</a></li>
+            <li><a href="MadLibs.php">Onkunde</a></li>
+        </ul>
+    </nav>
+
+    <h1> Onkunde </h1>
+
+    <p>Wat zou je graag willen kunnen? <input type="text"></p> 
+
+    <p>Met welke persoon kun je goed opschieten? <input type="text"></p>
+
+    <p>Wat is je favoriete getal? <input type="text"></p>
+
+    <p>Wat heb je altijd bij je als je op vakantie gaat? <input type="text"></p>
+
+    <p>Wat is je beste persoonlijke eigenschap? <input type="text"></p>
+
+    <p>Wat is je slechtste persoonlijke eigenschap? <input type="text"></p>
+
+    <p>Wat is het ergste dat je kan overkomen? <input type="text"></p>
+
+    <input type="button" value="Versturen">
+
+    <footer>
+        Deze website is gemaakt door Dewi van der Velden.
+    </footer>
+</div>
+
+ </body>
+</html>
+
