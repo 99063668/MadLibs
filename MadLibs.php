021b1b26a458ed3b1186c4001cba4d367c1e5d38
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-03-02 09:41:51 +0100

    Add files via upload

--- MadLibs.php
+++ MadLibs.php
@@ -1,26 +1,14 @@
-<?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $Input1 = $_POST["Input1"];
-        $Input2 = $_POST["Input2"];
-        $Input3 = $_POST["Input3"];
-        $Input4 = $_POST["Input4"];
-        $Input5 = $_POST["Input5"];
-        $Input6 = $_POST["Input6"];
-        $Input7 = $_POST["Input7"];
-
-        echo "Er zijn veel mensen die niet kunnen $Input1. Neem nou $Input2 Zelfs met de hulp van een $Input4 of zelfs $Input3 kan $Input2 niet $Input1. Dat heeft niet te maken met een gebrek aan $Input5 maar met een te veel aan $Input6. Te veel $Input6 leidt tot $Input7 en dat is niet goed als je wilt $Input1. Helaas voor $Input2";
-    }
-?>
-
-
 <!DOCTYPE html>
 <html lang="nl">
 <head>
    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
    <title>Mad Libs</title>
    <link rel="stylesheet" type="text/css" href="MadLibs.css">
+   <link rel="preconnect" href="https://fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&display=swap" rel="stylesheet">
 </head>
 <body>
+    <h1 class="text">Mad Libs</h1>
 <div class="container">
     <nav>
         <ul>
@@ -28,6 +16,22 @@
             <li><a href="MadLibs.php">Onkunde</a></li>
         </ul>
     </nav>
+
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $Input1 = $_POST["Input1"];
+            $Input2 = $_POST["Input2"];
+            $Input3 = $_POST["Input3"];
+            $Input4 = $_POST["Input4"];
+            $Input5 = $_POST["Input5"];
+            $Input6 = $_POST["Input6"];
+            $Input7 = $_POST["Input7"];
+
+            echo "Er zijn veel mensen die niet kunnen $Input1. Neem nou $Input2 Zelfs met de hulp van een $Input4 of zelfs
+            $Input3 kan $Input2 niet $Input1. Dat heeft niet te maken met een gebrek aan $Input5 maar met een te veel aan $Input6. 
+            Te veel $Input6 leidt tot $Input7 en dat is niet goed als je wilt $Input1. Helaas voor $Input2";
+        }
+    ?>
 
     <form action="" method="POST">
     <h1> Onkunde </h1>
@@ -49,9 +53,9 @@
     <input type="submit" name="submit" value="Versturen">
     </form>
 
-   <!-- <footer>
-       <p> Deze website is gemaakt door Dewi van der Velden.</p>
-    </footer>-->
+    <footer>
+       <p> Deze website is gemaakt door Dewi van der Velden @2021.</p>
+    </footer>
 </div>
 
  </body>
