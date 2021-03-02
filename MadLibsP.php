91a0709bc43a66a1f4e73aec7e1abcf7037abb3a
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-03-02 10:26:23 +0100

    Add files via upload

--- MadLibsP.php
+++ MadLibsP.php
@@ -17,40 +17,39 @@
         </ul>
     </nav>
 
-    <?php
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $Input1 = $_POST["Input1"];
-            $Input2 = $_POST["Input2"];
-            $Input3 = $_POST["Input3"];
-            $Input4 = $_POST["Input4"];
-            $Input5 = $_POST["Input5"];
-            $Input6 = $_POST["Input6"];
-            $Input7 = $_POST["Input7"];
-            $Input8 = $_POST["Input8"];
-
-            echo "Er heerst paniek in het koningkrijk $Input3 Koning $Input2 is ten einde raad en als koning $Input2 ten einde raad is, dan roept hij zijn ten-einde-raadsheer $Input6";
-            echo "<br>";
-            echo "<br> $Input6! Het is een ramp! Het is een schande!";
-            echo "<br>";
-            echo "<br> Sire, Majesteit, Uwe luidruchtigheid, wat is er aan de hand?";
-            echo "<br>";
-            echo "<br> Mijn $Input1 is verdwenen! Zo maar, zonder waarschuwing. En ik had net $Input5 voor hem gekocht!";
-            echo "<br>";
-            echo "<br> Majesteit, uw $Input1 komt vast vanzelf weer terug?";
-            echo "<br>";
-            echo "<br> Ja, da's leuk en aardig, maar hoe moet in in de tussentijd $Input8 leren?";
-            echo "<br>";
-            echo "<br> Maar Sire, daar kunt u toch uw $Input7 voor gebruiken.";
-            echo "<br>";
-            echo "<br> $Input2, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had.";
-            echo "<br>";
-            echo "<br> $Input4, Sire.";
-        }
-    ?>
-
-
     <form action="" method="POST">
         <h1> Er heerst paniek... </h1>
+
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                $Input1 = $_POST["Input1"];
+                $Input2 = $_POST["Input2"];
+                $Input3 = $_POST["Input3"];
+                $Input4 = $_POST["Input4"];
+                $Input5 = $_POST["Input5"];
+                $Input6 = $_POST["Input6"];
+                $Input7 = $_POST["Input7"];
+                $Input8 = $_POST["Input8"];
+
+                echo "Er heerst paniek in het koningkrijk $Input3 Koning $Input2 is ten einde raad en als koning $Input2 ten einde raad is, dan roept hij zijn ten-einde-raadsheer $Input6";
+                echo "<br>";
+                echo "<br> $Input6! Het is een ramp! Het is een schande!";
+                echo "<br>";
+                echo "<br> Sire, Majesteit, Uwe luidruchtigheid, wat is er aan de hand?";
+                echo "<br>";
+                echo "<br> Mijn $Input1 is verdwenen! Zo maar, zonder waarschuwing. En ik had net $Input5 voor hem gekocht!";
+                echo "<br>";
+                echo "<br> Majesteit, uw $Input1 komt vast vanzelf weer terug?";
+                echo "<br>";
+                echo "<br> Ja, da's leuk en aardig, maar hoe moet in in de tussentijd $Input8 leren?";
+                echo "<br>";
+                echo "<br> Maar Sire, daar kunt u toch uw $Input7 voor gebruiken.";
+                echo "<br>";
+                echo "<br> $Input2, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had.";
+                echo "<br>";
+                echo "<br> $Input4, Sire.";
+            }
+        ?>
 
         <p  id="Text">Welk dier zou je nooit als huisdier willen hebben? <input type="text" required name="Input1"></p> 
 
